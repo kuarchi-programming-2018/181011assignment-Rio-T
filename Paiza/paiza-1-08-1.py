6d8dc56671df551c93f6a598bcb3b84dbe33183e
Rio-T <43873898+Rio-T@users.noreply.github.com>
2018-10-07 15:07:36 +0900

    paiza2

--- Paiza/paiza-1-08-1.py
+++ Paiza/paiza-1-08-1.py
@@ -17,3 +17,7 @@ import random
 number = random.randint(1, 10)  # 匹数 1 ～ 10
 print("体重100キロのスライムが" + str(number) + "匹あらわれた")
 # 合計体重 = 匹数 x 100
+total=100*number
+print("スライムの合計体重は"+str(total)+"キロです")
+
+
